2ebed6f475a1745cf356c245ae28b21952f44292
BuildTools <unconfigured@null.spigotmc.org>
2023-01-29 01:31:01 +0100

    test

--- functions.php
+++ functions.php
@@ -28,3 +28,58 @@ function my_customizer_controls( $wp_customize ) {
   ) ) );
 }
 add_action( 'customize_register', 'my_customizer_controls' );
+
+function check_and_update_theme() {
+  // URL de l'API GitHub pour votre dépôt de thème
+  $api_url = 'https://api.github.com/repos/ludocaz/ludocaz_theme/releases/latest';
+
+  // Récupération des données de l'API GitHub
+  $response = wp_remote_get( $api_url );
+
+  // Vérification de la réponse de l'API
+  if( ! is_wp_error( $response ) ) {
+      $body = json_decode( $response['body'] );
+
+      // Vérification de la version de la dernière publication
+      if( version_compare( $body->tag_name, wp_get_theme()->get( 'Version' ), '>' ) ) {
+
+          // Vérifie les autorisations d'écriture
+          if( is_writable( WP_CONTENT_DIR . '/themes/' ) ) {
+
+              // Téléchargement de l'archive de la dernière publication
+              $zip_url = $body->zipball_url;
+              $zip_file = download_url( $zip_url );
+
+              // Extraction de l'archive
+              $path = WP_CONTENT_DIR . '/themes/';
+              $unzip = unzip_file( $zip_file, $path );
+
+              // Vérification de l'extraction
+              if( ! is_wp_error( $unzip ) ) {
+                  // suppression de l'archive
+                  unlink( $zip_file );
+
+                  // Mise à jour de la base de données
+                  $theme = wp_get_theme();
+                  update_option( 'template', $theme->get_template() );
+                  update_option( 'stylesheet', $theme->get_stylesheet() );
+
+                  // Affiche un message de succès
+                  echo 'Thème mis à jour avec succès';
+              } else {
+                  // Affiche un message d'erreur
+                  echo 'Erreur lors de l\'extraction de l\'archive';
+              }
+          } else {
+              // Affiche un message d'erreur
+              echo 'Le répertoire des thèmes n\'est pas accessible en écriture';
+          }
+      }
+  }
+}
+
+// Ajout de la tâche cron pour vérifier les mises à jour de thème toutes les heures
+if ( ! wp_next_scheduled( 'check_and_update_theme_hook' ) ) {
+  wp_schedule_event( time(), 'hourly', 'check_and_update_theme_hook' );
+}
+add_action( 'check_and_update_theme_hook', 'check_and_update_theme' );
